3255e8646984c2bdee98414ed181405011541133
Kethurk <106808085+Kethurk@users.noreply.github.com>
2026-07-29 14:43:05 +0200

    RadiationArea finished

--- Scripts/RadiationArea/SCR_RadiationDamageArea.c
+++ Scripts/RadiationArea/SCR_RadiationDamageArea.c
@@ -1,7 +1,15 @@
+[BaseContainerProps()]
 class SCR_RadiationDamageArea : SCR_DamageArea
 {
 	[Atribute()]
 	float m_fEmissionRate;
+	
+	//------------------------------------------------------------------------------------------------
+	//! \return
+	override bool IsRegisteredAutomatically()
+	{
+		return true;
+	}
 	
 	//------------------------------------------------------------------------------------------------
 	//! Callback when an entity enters this DamageArea this frame
@@ -17,10 +25,8 @@ class SCR_RadiationDamageArea : SCR_DamageArea
 		 if (allHitZones.IsEmpty())
 			return;
 		
-		//bool effectApplied = IsEffectApplied(entity);
 		array<ref SCR_PersistentDamageEffect> damageEffects = {};
-		if (damageManager.FindAllDamageEffectsOfType(GetDamageEffect().Type(), damageEffects) < 1)
-			return false;
+		damageManager.FindAllDamageEffectsOfType(GetDamageEffect().Type(), damageEffects);
 		
 		map<HitZone, SCR_PersistentDamageEffect> allHitzonesAffected = new map<HitZone, SCR_PersistentDamageEffect>();
 		foreach (SCR_PersistentDamageEffect dmgEffect : damageEffects)
@@ -34,9 +40,8 @@ class SCR_RadiationDamageArea : SCR_DamageArea
 			if (!allHitzonesAffected.Contains(hitZone))
 			{
 				radiationDamage = new SCR_RadiationDamageEffect;
-				
-				// Set hitzone and add the effect
-				Print("Adding effect");
+				radiationDamage.SetAffectedHitZone(hitZone);
+				AddDamage(radiationDamage);
 			}
 			else
 			{
@@ -52,10 +57,34 @@ class SCR_RadiationDamageArea : SCR_DamageArea
 	//! \param[in] entity
 	override void OnAreaExit(IEntity entity)
 	{
-		// Remove here refs to the owner
-		super.OnAreaExit(entity);
+		SCR_ExtendedDamageManagerComponent damageManagerExt = SCR_ExtendedDamageManagerComponent.Cast(SCR_DamageManagerComponent.GetDamageManager(entity));
+		if (!damageManagerExt)
+			return;
 		
+		array<HitZone> allHitZones = {};
+		damageManagerExt.GetAllHitZones(allHitZones);
+		 if (allHitZones.IsEmpty())
+			return;
 		
+		array<ref SCR_PersistentDamageEffect> damageEffects = {};
+		if (damageManager.FindAllDamageEffectsOfType(GetDamageEffect().Type(), damageEffects) < 1)
+			return false;
+		
+		map<HitZone, SCR_PersistentDamageEffect> allHitzonesAffected = new map<HitZone, SCR_PersistentDamageEffect>();
+		foreach (SCR_PersistentDamageEffect dmgEffect : damageEffects)
+		{
+			allHitzonesAffected.Insert(dmgEffect.GetAffectedHitZone(), dmgEffect);
+		}
+		
+		foreach (HitZone hitZone : allHitZones)
+		{
+			SCR_RadiationDamageEffect radiationDamage;
+			if (allHitzonesAffected.Contains(hitZone))
+			{
+				radiationDamage = allHitzonesAffected.Get(hitZone);
+				radiationDamage.RemoveRadiationArea(GetOwner());
+			}
+		}
 	}
 	
 	//------------------------------------------------------------------------------------------------
