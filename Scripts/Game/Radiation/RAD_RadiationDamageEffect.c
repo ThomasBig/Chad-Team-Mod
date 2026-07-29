bde2b006ec1b2db34f41fb746eb87c538a0dbe59
David Brož <david.broz.official@gmail.com>
2026-07-29 16:23:59 +0200

    balance

--- Scripts/Game/Radiation/RAD_RadiationDamageEffect.c
+++ Scripts/Game/Radiation/RAD_RadiationDamageEffect.c
@@ -3,7 +3,7 @@ class RAD_RadiationDamageEffect : SCR_PersistentDamageEffect
 	ref array<RAD_RadiationDamageArea> m_aRadiationAreas = {};
 	protected float m_fRadiationPoisoningLevel = 0; //! Amount of accumulated radiation damage consumed by the hit zone (abstract unit)
 	protected float m_fRadiationPoisoningLevelDecay = 5; // Amount of radiation damage (abstract unit) the body self repair per seconds
-	protected const float DAMAGE_PER_POISON_LEVEL = 0.0001;
+	protected const float DAMAGE_PER_POISON_LEVEL = 0;
 	protected const float EXPOSURE_PER_DISTACE = 1; 
 	
 	protected IEntity m_lastInstigator;
@@ -36,7 +36,7 @@ class RAD_RadiationDamageEffect : SCR_PersistentDamageEffect
 		}
 		
 		// Radiation Poisoning Decay (linear)
-		m_fRadiationPoisoningLevel = m_fRadiationPoisoningLevel - m_fRadiationPoisoningLevelDecay * timeSlice;
+		m_fRadiationPoisoningLevel = Math.Max(m_fRadiationPoisoningLevel - m_fRadiationPoisoningLevelDecay * timeSlice, 0);
 		
 		// Damage to the hit zone
 		float damageAmount = m_fRadiationPoisoningLevel * DAMAGE_PER_POISON_LEVEL;
