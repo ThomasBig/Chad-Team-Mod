229103730de3bc418ee386621b366ce9517353d9
David Brož <david.broz.official@gmail.com>
2026-07-29 16:20:06 +0200

    Balancing

--- Scripts/Game/Radiation/RAD_RadiationDamageEffect.c
+++ Scripts/Game/Radiation/RAD_RadiationDamageEffect.c
@@ -3,7 +3,7 @@ class RAD_RadiationDamageEffect : SCR_PersistentDamageEffect
 	ref array<RAD_RadiationDamageArea> m_aRadiationAreas = {};
 	protected float m_fRadiationPoisoningLevel = 0; //! Amount of accumulated radiation damage consumed by the hit zone (abstract unit)
 	protected float m_fRadiationPoisoningLevelDecay = 5; // Amount of radiation damage (abstract unit) the body self repair per seconds
-	protected const float DAMAGE_PER_POISON_LEVEL = 1;
+	protected const float DAMAGE_PER_POISON_LEVEL = 0.0001;
 	protected const float EXPOSURE_PER_DISTACE = 1; 
 	
 	protected IEntity m_lastInstigator;
@@ -29,7 +29,7 @@ class RAD_RadiationDamageEffect : SCR_PersistentDamageEffect
 			m_lastInstigator = source.GetParent();
 			
 			int soucreTraceIndex = tracedEntities.Get(tracedEntities.Count()-1);
-			int distaceToSource = tracedDistances.Get(soucreTraceIndex); // units?
+			float distaceToSource = tracedDistances.Get(soucreTraceIndex); // units?
 			float exposureLevel = EXPOSURE_PER_DISTACE * distaceToSource * timeSlice; //db TODO: Change this for caluclation based on the trace
 			
 			m_fRadiationPoisoningLevel += exposureLevel;
