70348d38f27d28efc48d96d2ea35d3fcedeb047b
David Brož <david.broz.official@gmail.com>
2026-07-29 15:36:15 +0200

    Adding tracing loop

--- Scripts/Game/Radiation/RAD_RadiationDamageEffect.c
+++ Scripts/Game/Radiation/RAD_RadiationDamageEffect.c
@@ -1,7 +1,8 @@
 class RAD_RadiationDamageEffect : SCR_PersistentDamageEffect
 {
 	ref array<IEntity> m_aRadiationAreas = {};
-	protected float m_fRadiationPoisonLevel = 0; //! Amount of radiation damage consumed by the hit zone 
+	protected float m_fRadiationPoisoningLevel = 0; //! Amount of accumulated radiation damage consumed by the hit zone (abstract unit)
+	protected float m_fRadiationPoisoningLevelDecay = 5; // Amount of radiation damage (abstract unit) the body self repair per seconds
 	
 	//------------------------------------------------------------------------------------------------
 	override event void EOnFrame(float timeSlice, SCR_ExtendedDamageManagerComponent dmgManager)
@@ -14,8 +15,13 @@ class RAD_RadiationDamageEffect : SCR_PersistentDamageEffect
 		foreach (IEntity source : m_aRadiationAreas)
 		{
 			SCR_Trace sourceTrace = new SCR_Trace();
-			
+			array<IEntity> tracedEntities = {};
+			array<GameMaterial> tracedMaterials = {};
+			sourceTrace.TraceFromEntityToHitzone(source, dmgManager.GetOwner(), affectedHitZone, tracedEntities, tracedMaterials);
 		}
+		
+		// Radiation Poisoning Decay
+		
 	}
 	
 	//------------------------------------------------------------------------------------------------
