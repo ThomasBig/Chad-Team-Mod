b6501c3b165d2c5debef579fb550aa46d1bbdc29
Kate Beck <tomas.janousek@bistudio.com>
2026-07-29 14:09:13 +0200

    Radiation UI

---
+++ scripts/Game/UI/HUD/RadiationEffect.c
@@ -0,0 +1,16 @@
+//! Handles UI displaying mortar values
+//! - azimuth, elevation, range
+class RAD_RadiationInfo : SCR_InfoDisplayExtended
+{
+	protected ref RAD_RadiationInfoWidgets m_Widgets = new RAD_RadiationInfoWidgets();
+
+	//------------------------------------------------------------------------------------------------
+	override void DisplayStartDraw(IEntity owner)
+	{
+		if (!m_wRoot)
+			return;
+		
+		m_Widgets = new RAD_RadiationInfoWidgets();
+		m_Widgets.Init(m_wRoot);
+	}
+}
